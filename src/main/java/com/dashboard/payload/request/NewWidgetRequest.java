02edc55a5714738137e1abb8ce57e974818f2f7e
Antoine <antoine.brld@gmail.com>
2020-12-16 15:13:06 +0100

    git add files

--- src/main/java/com/dashboard/payload/request/NewWidgetRequest.java
+++ src/main/java/com/dashboard/payload/request/NewWidgetRequest.java
@@ -1,18 +1,30 @@
 package com.dashboard.payload.request;
 
 import com.dashboard.models.Widget;
+import com.dashboard.models.services.Param;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class SaveWidgetRequest {
+public class NewWidgetRequest {
 
-    private List<Widget> widgets;
+    private String name;
+    private Map<String, String> params = new HashMap<>();
 
-    public List<Widget> getWidgets() {
-        return widgets;
+    public String getName() {
+        return name;
     }
 
-    public void setWidgets(List<Widget> widgets) {
-        this.widgets = widgets;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
     }
 }
--- src/main/java/com/dashboard/payload/request/SaveWidgetRequest.java
+++ src/main/java/com/dashboard/payload/request/SaveWidgetRequest.java
@@ -1,53 +1,19 @@
 package com.dashboard.payload.request;
 
-import javax.validation.constraints.*;
-import java.util.Set;
+import com.dashboard.models.Widget;
 
-public class SignupRequest {
-    @NotBlank
-    @Size(min = 3, max = 20)
-    private String username;
+import java.util.ArrayList;
+import java.util.List;
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    private String email;
+public class SaveWidgetRequest {
 
-    private Set<String> roles;
+    private List<Widget> widgets;
 
-    @NotBlank
-    @Size(min = 6, max = 40)
-    private String password;
-
-    public String getUsername() {
-        return username;
+    public List<Widget> getWidgets() {
+        return widgets;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<String> getRoles() {
-        return this.roles;
-    }
-
-    public void setRole(Set<String> roles) {
-        this.roles = roles;
+    public void setWidgets(List<Widget> widgets) {
+        this.widgets = widgets;
     }
 }
